435f17fadcd3fd529b23e2edee3d0884a9f6f8d5
Patel Het <hetbpatel2906@gmail.com>
2026-06-13 13:07:30 +0530

    Initial commit

--- forms.py
+++ forms.py
@@ -15,7 +15,7 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email=StringField('Email', validators=[DataRequired(), Email()])
-    password=StringField('Password', validators=[DataRequired()])
+    password=PasswordField('Password', validators=[DataRequired()])
     remember=BooleanField('Remember Me')
     submit=SubmitField('Login')
     
